91fe97108c4844e7321a9953558da5b84a05216e
Chris Alfano <chris@jarv.us>
2018-12-09 08:58:44 +0000

    feat: add defaults for maximumLevel and minimumAverage

---
+++ php-config/Git.config.d/b21-skeleton.php
@@ -0,0 +1,10 @@
+<?php
+
+Git::$repositories['b21-skeleton'] = [
+    'remote' => 'git@git.jarv.us:b21/b21-skeleton.git',
+    'originBranch' => 'master',
+    'workingBranch' => 'master',
+    'trees' => [
+        'php-config'
+    ]
+];
