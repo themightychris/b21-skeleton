1c8490d2ee5cdf1c34d26e5370621b6c032e949d
Nafis Bey <nafis@jarv.us>
2019-10-04 01:20:55 +0000

    refactor: use connector class in event handler

--- event-handlers/Slate/CBL/export-data/50_data-warehouse-export.php
+++ event-handlers/Slate/CBL/export-data/50_data-warehouse-export.php
@@ -1,3 +1,17 @@
 <?php
 
-\Slate\CBL\DataWarehouseExporter::exportData();
+$Job = \Emergence\Connectors\Job::create([
+    'Connector' => \Slate\Connectors\DataWarehouse\Connector::class,
+    'Config' => [
+        'Pdo' => \Slate\CBL\DataWarehouseExporter::getPdo(),
+        'exports' => [
+            'slate/terms',
+            'slate-cbl/demonstrations',
+            'slate-cbl/student-competencies',
+            'slate-cbl/student-portfolios',
+            'slate-cbl/student-tasks'
+        ]
+    ]
+]);
+
+\Slate\Connectors\DataWarehouse\Connector::synchronize($Job, false);
