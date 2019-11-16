c8edcd4baa87e53702052c488ec174ae213c3373
Nafis Bey <nafis@nafis.me>
2019-11-15 19:01:40 -0500

    fix: variable type

--- php-config/Slate/Connectors/DataWarehouse/Connector.config.php
+++ php-config/Slate/Connectors/DataWarehouse/Connector.config.php
@@ -16,6 +16,6 @@ Connector::$exports = [
     'slate-cbl/student-portfolios' => $exporters['slate-cbl/student-portfolios'],
     'slate-cbl/student-competencies' => $exporters['slate-cbl/student-competencies'],
     'slate-cbl/student-tasks' => $exporters['slate-cbl/student-tasks'],
-    'slate/terms' => $exports['slate/terms'],
+    'slate/terms' => $exporters['slate/terms'],
     'slate-cbl/demonstrations' => $exporters['slate-cbl/demonstrations']
 ];
