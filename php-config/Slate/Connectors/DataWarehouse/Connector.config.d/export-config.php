eddf137f9245c8237a7702d767be5a9bedaba822
Nafis Bey <nafis@nafis.me>
2019-11-16 20:44:51 -0500

    refactor: connector config files

---
+++ php-config/Slate/Connectors/DataWarehouse/Connector.config.d/export-config.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Slate\Connectors\DataWarehouse;
+
+
+/* Enable & Comment out a line below to disable a specific export */
+
+// $exporters = Connector::$exports;
+// Connector::$exports = [
+//     'slate-cbl/student-portfolios' => $exporters['slate-cbl/student-portfolios'],
+//     'slate-cbl/student-competencies' => $exporters['slate-cbl/student-competencies'],
+//     'slate-cbl/student-tasks' => $exporters['slate-cbl/student-tasks'],
+//     'slate/terms' => $exporters['slate/terms'],
+//     'slate-cbl/demonstrations' => $exporters['slate-cbl/demonstrations']
+// ];
---
+++ php-config/Slate/Connectors/DataWarehouse/Connector.config.d/postgres-config.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Slate\Connectors\DataWarehouse;
+
+Connector::$postgresHost;
+Connector::$postgresPort;
+Connector::$postgresUsername;
+Connector::$postgresPassword;
+Connector::$postgresDatabase;
+Connector::$postgresSchema;
